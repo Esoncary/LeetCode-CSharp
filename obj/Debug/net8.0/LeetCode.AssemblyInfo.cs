396b713426a2fe660409f09e5ac45ada9c0ec939
CloudsMaker <1418419357@qq.com>
2025-04-17 23:36:17 +0800

    test ignore

--- obj/Debug/net8.0/LeetCode.AssemblyInfo.cs
+++ obj/Debug/net8.0/LeetCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f191abe704397165bcf583c9641cca5f92e958f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8629ccf93be85543abb20a941ec33c634660ff1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
