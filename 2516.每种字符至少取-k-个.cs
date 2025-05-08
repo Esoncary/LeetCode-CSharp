a9589a57c016db86ca9f145c1b441e370c835f9b
CloudsMaker <1418419357@qq.com>
2025-05-08 11:48:10 +0800

    123

---
+++ 2516.每种字符至少取-k-个.cs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=2516 lang=csharp
+ *
+ * [2516] 每种字符至少取 K 个
+ */
+
+// @lc code=start
+public class Solution {
+    public int TakeCharacters(string s, int k) {
+        
+    }
+}
+// @lc code=end
+
